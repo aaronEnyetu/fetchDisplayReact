b97ae3c6a8da8aadab675679c48b836781253c34
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-21 13:20:33 -0700

    Mapped the food list

--- src/FoodsList.js
+++ src/FoodsList.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+export default function FoodsList({ foods = [] }) {
+  return (
+    <div className='foods-list'>
+      {
+        foods.map((food, i) => <div className='food' key={food.name + food.id + i}>
+          <h2>{food.name}</h2>
+          <ul>{food.nutrients.map((nutrient, i) => <li key={nutrient + i}> {nutrient}</li>)}</ul>
+        </div>)
+      }
+    </div>
+  );
+}
