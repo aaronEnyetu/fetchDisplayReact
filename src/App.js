b521db476e7b8f47cb1c8d2860fb46602cd02d29
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-21 12:12:30 -0700

    Set state plants array and mapped

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getPlants } from './service/fetch-utils';
 import './App.css';
 // import your arrays here
+import PlantsList from './PlantsList';
+
+
 
 function App() {
+  //Data from supabase lives in state
+
+  const [plants, setPlants] = useState([]);
+
+
+  async function fetchPlantsData() {
+
+    const data = await getPlants();
+    setPlants(data);
+  }
+
+  useEffect(() => {
+    fetchPlantsData();
+  }, []);
+
+
+
+  
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
+      <PlantsList plants={plants}/>
     </div>
   );
 }
